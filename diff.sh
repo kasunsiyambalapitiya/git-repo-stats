a96f0c36be3a31e1db12b3632650d3b14e5b87d8
kasun <kasunsiyambalapitiya@gmail.com>
2016-12-02 12:15:59 +0530

    diff.sh is modified only to save the diffs to the relevant files

--- diff.sh
+++ diff.sh
@@ -6,25 +6,26 @@
 countModification(){
 mkdir -p tempFiles
 #count the no of modifications
-git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="!")print $1;}' > ./tempFiles/diffChanges.txt
+git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="!")print $1;}' >> ./tempFiles/diffChanges.txt
 
 #count the no of additions
-git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="+")print $1;}' > ./tempFiles/diffAdditions.txt
+git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="+")print $1;}' >> ./tempFiles/diffAdditions.txt
 
 #count the no of deletions
-git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="-")print $1;}' > ./tempFiles/diffDeletions.txt
+git difftool -y -x "diff -c" $1 $2 | awk '{if ($1=="-")print $1;}' >>./tempFiles/diffDeletions.txt
+
+#below lines are commented out as this is now only been used to save the diff to the relevant files
+# for testing echoing the no of lines
+#noChanges=$(cat ./tempFiles/diffChanges.txt | wc -l)
+#echo "no of changes" $noChanges
 
 # for testing echoing the no of lines
-noChanges=$(cat ./tempFiles/diffChanges.txt | wc -l)
-echo "no of changes" $noChanges
+#noAdditions=$(cat ./tempFiles/diffAdditions.txt | wc -l)
+#echo "no of additions" $noAdditions
 
 # for testing echoing the no of lines
-noAdditions=$(cat ./tempFiles/diffAdditions.txt | wc -l)
-echo "no of additions" $noAdditions
-
-# for testing echoing the no of lines
-noDeletions=$(cat ./tempFiles/diffDeletions.txt | wc -l)
-echo "no of deletions" $noDeletions
+#noDeletions=$(cat ./tempFiles/diffDeletions.txt | wc -l)
+#echo "no of deletions" $noDeletions
 
 
 }
